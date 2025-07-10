72a11f21ad814183e7dfde3786906ee2f0c183b8
Mostary Jahan <155467590+mostaryjahan@users.noreply.github.com>
2025-07-10 18:05:14 +0600

    add user interfaces, model, controller

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,38 @@
+import { Types } from "mongoose";
+
+export enum Role {
+  SUPER_ADMIN = "SUPER_ADMIN",
+  ADMIN = "ADMIN",
+  USER = "USER",
+  GUIDE = "GUIDE",
+}
+
+// auth providers
+// email, google
+
+export interface IAuthProvider {
+  provider: string; //google,email
+  providerId: string;
+}
+
+export enum IsActive {
+  ACTIVE = "ACTIVE",
+  INACTIVE = "INACTIVE",
+  BLOCKED = "BLOCKED",
+}
+
+export interface IUser {
+  name: string;
+  email: string;
+  password?: string;
+  phone?: string;
+  picture?: string;
+  address?: string;
+  isDeleted?: string;
+  isActive?: IsActive;
+  isVerified?: string;
+  role: Role;
+  auths: IAuthProvider[];
+  bookings?: Types.ObjectId[];
+  guides?: Types.ObjectId[];
+}

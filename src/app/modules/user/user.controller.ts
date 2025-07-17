6f996731554a5c36cbe6b7344f55f0cd88a4a2c4
Mostary Jahan <155467590+mostaryjahan@users.noreply.github.com>
2025-07-17 12:26:58 +0600

    add

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -28,11 +28,15 @@ const createUser = catchAsync(
 const updateUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const userId = req.params.id;
-    const token = req.headers.authorization;
-    const verifiedToken = verifyToken(
-      token as string,
-      envVars.JWT_ACCESS_SECRET
-    ) as JwtPayload;
+
+    // const token = req.headers.authorization;
+    // const verifiedToken = verifyToken(
+    //   token as string,
+    //   envVars.JWT_ACCESS_SECRET
+    // ) as JwtPayload;
+    const verifiedToken = req.user;
+
+
     const payload = req.body;
 
     const user = await UserServices.updateUser(userId, payload, verifiedToken);
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { UserController } from "./user.controller";
-import { createUserZodSchema } from "./user.validation";
+import { createUserZodSchema, updateUserZodSchema } from "./user.validation";
 import { validateRequest } from "../../middlewares/validateRequest";
 import { checkAuth } from "../../middlewares/checkAuth";
 import { Role } from "./user.interface";
@@ -20,6 +20,6 @@ router.get(
   UserController.getAllUser
 );
 
-router.patch("/:id", checkAuth(...Object.values(Role)), UserController.updateUser)
+router.patch("/:id",validateRequest(updateUserZodSchema), checkAuth(...Object.values(Role)), UserController.updateUser)
 
 export const UserRoutes = router;

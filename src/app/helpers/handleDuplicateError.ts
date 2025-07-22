c087bba66e626803fb6e7816e33edc1664ca1fdd
Mostary Jahan <155467590+mostaryjahan@users.noreply.github.com>
2025-07-22 17:57:58 +0600

    added division and tour

--- src/app/helpers/handleDuplicateError.ts
+++ src/app/helpers/handleDuplicateError.ts
@@ -1,10 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { TGenericErrorResponse } from "../interfaces/error.types";
 
-export const handleDuplicateError = (err: any):TGenericErrorResponse => {
-  const matchedArray = err.message.match(/"([^"]*)"/);
+
+export const handleDuplicateError = (err: any): TGenericErrorResponse => {
+  const matchedArray = err.message?.match(/"([^"]*)"/);
+  const field = matchedArray?.[1] || "Field";
+  
   return {
     statusCode: 400,
-    message: `${matchedArray[1]} already exists`,
+    message: `${field} already exists`,
   };
 };
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,21 +1,30 @@
-import { Router } from "express"
-import { UserRoutes } from "../modules/user/user.route"
-import { AuthRoutes } from "../modules/auth/auth.route"
+import { Router } from "express";
+import { UserRoutes } from "../modules/user/user.route";
+import { AuthRoutes } from "../modules/auth/auth.route";
+import { DivisionRoutes } from "../modules/division/division.route";
+import { TourRoutes } from "../modules/tour/tour.route";
 
-export const router = Router()
+export const router = Router();
 
 const moduleRoutes = [
-    {
-        path: "/user",
-        route: UserRoutes
-    },
-     {
-        path: "/auth",
-        route: AuthRoutes
-    }
-]
-
+  {
+    path: "/user",
+    route: UserRoutes,
+  },
+  {
+    path: "/auth",
+    route: AuthRoutes,
+  },
+  {
+    path: "/division",
+    route: DivisionRoutes,
+  },
+  {
+    path: "/tour",
+    route: TourRoutes,
+  },
+];
 
 moduleRoutes.forEach((route) => {
-    router.use(route.path, route.route)
-})
+  router.use(route.path, route.route);
+});

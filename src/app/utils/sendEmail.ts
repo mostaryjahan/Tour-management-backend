a5ddae8043163a924959322b28259c1d9055252d
Mostary Jahan <155467590+mostaryjahan@users.noreply.github.com>
2025-08-06 22:16:06 +0600

    add redis

--- src/app/utils/sendEmail.ts
+++ src/app/utils/sendEmail.ts
@@ -49,7 +49,7 @@ export const sendEmail = async ({
                 contentType: attachment.contentType
             }))
         })
-        // console.log(`\u2709\uFE0F Email sent to ${to}: ${info.messageId}`);
+         console.log(`\u2709\uFE0F Email sent to ${to}: ${info.messageId}`);
     } catch (error: any) {
         console.log("email sending error", error.message);
         throw new AppError(401, "Email error")
